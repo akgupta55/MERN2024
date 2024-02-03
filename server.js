a3c8b0e5a6894a55841f30e3e0a1ef12880d3491
Ajay Kumar Gupta <rg190559@gmail.com>
2024-02-03 23:42:45 +0530

    adding dotenv and connecting with atlas dabase

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const router = require("./router/auth-route");
+const connectDb = require("./util/db");
 
 app.use(express.json());
 
@@ -9,6 +11,10 @@ app.use("/api/auth", router);
 
 const PORT = 5000;
 
-app.listen(PORT, () => {
-  console.log(`server is running at port : ${PORT}`);
+connectDb().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server is running at port : ${PORT}`);
+  });
 });
+
+// k7APEJ2Nn98mV0nb
